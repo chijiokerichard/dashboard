762ae09bf9ec655ba2a5be81a4ce149b1cab3e86
chijiokerichard <chijiokrichard61@gmail.com>
2024-07-05 00:35:07 +0100

    i deleted components folder

--- app/components/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import ContextProvider from "./ContextProvider";
-
-export default function Layout({children}: {children: React.ReactNode}) {
-    return (
-      <ContextProvider>
-        <body>{children}</body>
-      </ContextProvider>
-    );
-  }
